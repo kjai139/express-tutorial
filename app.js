efe7de39fca762e2694b0c59494bd0082de778e8
simon y <71997308+kjai139@users.noreply.github.com>
2023-05-18 01:31:52 -0400

    tutorial p4

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const userCoolRouter = require('./routes/cool')
+const catalogRouter = require('./routes/catalog')
 
 require('dotenv').config()
 
@@ -37,6 +38,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/users/cool', userCoolRouter)
+app.use('/catalog', catalogRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
